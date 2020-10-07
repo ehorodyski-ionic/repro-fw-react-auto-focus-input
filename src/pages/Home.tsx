e18ae0fd68b77c55e7df57027e734e6567ee1e13
Eric Horodyski <horodyski@ionic.io>
2020-10-07 10:26:18 -0400

    Home is now class-based component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,9 +9,64 @@ import {
   IonItem,
   IonLabel,
 } from "@ionic/react";
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 import AutoFocusInput from "../components/AutoFocusInput";
 import "./Home.css";
+
+type HomeState = {
+  username: string;
+  password: string;
+};
+
+export class HomeClassComponent extends Component<{}, HomeState> {
+  constructor(props: any) {
+    super(props);
+    this.state = { username: "", password: "" };
+  }
+
+  render() {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Auto Focus Input</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+          <IonHeader collapse="condense">
+            <IonToolbar>
+              <IonTitle size="large">Auto Focus Input</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+
+          <form>
+            <IonItem>
+              <IonLabel position="stacked">Username</IonLabel>
+              <AutoFocusInput
+                type="text"
+                onIonChange={(e: any) =>
+                  this.setState({ username: e.detail.value! })
+                }
+              />
+            </IonItem>
+            <IonItem>
+              <IonLabel position="stacked">Password</IonLabel>
+              <IonInput
+                type="password"
+                onIonChange={(e) =>
+                  this.setState({ password: e.detail.value! })
+                }
+              />
+            </IonItem>
+            <IonButton expand="full">Test</IonButton>
+          </form>
+          <div>{this.state && this.state.username}</div>
+          <div>{this.state && this.state.password}</div>
+        </IonContent>
+      </IonPage>
+    );
+  }
+}
 
 const Home: React.FC = () => {
   const [username, setUsername] = useState<string>("");
